1de5a5ee7086084da9d806bd76ade0f8c0ada453
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2025-01-21 14:00:03 -0600

    test

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ const client = new Client({
   ],
 });
 
+client.on("ready", async () => {
+  let servers = client.guilds.cache.size;
+  let servercount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0);
+  console.log(
+    `Ready! Logged in as ${client.user.username}\ncurrently in ${servers} servers and watching over ${servercount} members`
+  );
+  console.log(`Ping: ${client.ws.ping}`);
+
+  client.user.setPresence({
+    activities: [{ name: "meow", type: 0 }],
+  });
+});
+
 module.exports = client;
 client.slashCommands = new Collection();
 client.commands = new Collection();
